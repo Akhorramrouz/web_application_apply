108ae767e170923cc2a194948d27d8c688c582bc
Akhorramrouz <adel.khorramrouz@gmail.com>
2022-08-02 02:18:56 +0430

    update

--- db.py
+++ db.py
@@ -15,4 +15,10 @@ def update_db(df):
     sh = gc.open_by_key("1sX_GB2E5MazKsafxF1qLWkUMVjrlf5FljwYyghMPnic")
     worksheet = sh.sheet1
     set_with_dataframe(worksheet,df)
+
+def save_users_extracted_data(username,final_data):
+    gc = gspread.service_account('apply4fund-db-e189a230919b.json')
+    sh = gc.open_by_key("1sX_GB2E5MazKsafxF1qLWkUMVjrlf5FljwYyghMPnic")
+    sh.add_worksheet(username,1000,30)
+    set_with_dataframe(sh.get_worksheet(-1), final_data)
     
